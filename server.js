15cbed3b50f2d97bae1c81617603d205ff1b2728
SimonGPort <simon.giroux.portelance@gmail.com>
2020-10-01 23:16:20 -0400

    cheerios

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 let express = require('express')
 let app = express()
 let route = express.Router()
+const fetch = require('node-fetch')
+const cheerio = require('cheerio')
 // let reloadMagic = require('./reload-magic.js')
 
 // reloadMagic(app)
+
+const url = 'https://www.koodomobile.com/fr/rate-plans?INTCMP=KMNew_NavMenu_Shop_Plans'
+
 
 
 
@@ -13,12 +18,26 @@ let route = express.Router()
 app.use('/', express.static('build')); // Needed for the HTML and JS files
 app.use('/', express.static('public')); // Needed for local assets
 
-app.get("/test", function (req, res, next) {
-    res.json([
-        { id: 1, username: "somebody" },
-        { id: 2, username: "somebody_else" }
-    ])
+app.get("/test", async function (req, res, next) {
+    let response = "test"
+
+    response = await this.getTheHTML(url)
+    console.log(response)
+    let $ = cheerio.load(response)
+    let array = []
+    $('.koodo-currency').each(function (i, element) {
+        console.log(i)
+        let $element = $(element)
+        array.push($element.text())
+    })
+
+    res.json(array)
 })
+
+getTheHTML = (url) => {
+
+    return fetch(`${url}`).then(value => value.text())
+}
 
 // Your endpoints go after this line
 
