314f3a1146dda5b841b45d03dc0b6e1f3661fe8b
SimonGPort <simon.giroux.portelance@gmail.com>
2020-10-24 22:05:27 -0400

    scrap bell partie 1

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const cheerio = require('cheerio')
 
 const urlKoodo = 'https://www.koodomobile.com/rate-plans'
 
+const urlBell1 = 'https://www.bell.ca/Mobility/Cell_phone_plans'
+const urlBell2 = 'https://www.bell.ca/Mobility/Cell_phone_plans/Promotional-plans'
+const urlBell3 = 'https://www.bell.ca/Mobility/Cell_phone_plans/Basic-Rate-Plans'
 
 
 
@@ -19,6 +22,7 @@ const urlKoodo = 'https://www.koodomobile.com/rate-plans'
 app.use('/', express.static('build')); // Needed for the HTML and JS files
 app.use('/', express.static('public')); // Needed for local assets
 
+// ----Koodo----
 app.get("/koodo", async function (req, res, next) {
     let response = await this.getTheHTML(urlKoodo)
     let $ = cheerio.load(response)
@@ -62,6 +66,65 @@ app.get("/koodo", async function (req, res, next) {
         return;
     }
 })
+
+// ----BELL----
+app.get("/bell", async function (req, res, next) {
+    let response1 = await this.getTheHTML(urlBell1)
+    let response2 = await this.getTheHTML(urlBell2)
+    let response3 = await this.getTheHTML(urlBell3)
+
+    let $ = cheerio.load(response1)
+    try {
+        let arrayPrice = []
+        $('.rsx-price').children('span').each(function (i, element) {
+            let $element = $(element)
+            arrayPrice.push($element.text())
+        })
+        let arrayCondition = []
+        let arrayTime = []
+        let arrayText = []
+        $('.rp-plan-features').children('dl').each(function (i, element) {
+            let $element = $(element)
+            let result = $element.text()
+            if (result.includes("Data")) {
+                result = result.trim().replace("Data", "").trim()
+                arrayCondition.push(result)
+            }
+            if (result.includes("Talk")) {
+                result = result.trim().replace("Talk", "").trim()
+                arrayTime.push(result)
+            }
+            if (result.includes("Text")) {
+                result = result.trim().replace("Text", "").trim()
+                arrayText.push(result)
+            }
+        })
+
+        let result = []
+        arrayPrice.forEach((value, idx) => {
+            let container = {}
+            container.price = arrayPrice[idx]
+            container.condition = arrayCondition[idx]
+            container.time = arrayTime[idx]
+            container.text = arrayText[idx]
+            result.push(container)
+        })
+        result = result.filter((plan) => {
+            return plan.condition
+        })
+
+        res.json(result)
+    }
+    catch (err) {
+        console.log("error", err);
+        res.send(JSON.stringify({ success: false }));
+        return;
+    }
+})
+
+
+
+
 
 getTheHTML = (url) => {
 
